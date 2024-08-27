3e6ffe96ae1738169f9e8f842ac322b026928b9e
Vinoth Kannan S <117577873+vinoth-kannan@users.noreply.github.com>
2024-08-27 18:45:26 +0530

    Delete Week4 directory

--- Week4/Exercise 6/BookstoreAPI/src/main/java/com/example/BookstoreAPI/service/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookstoreAPI.service;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
-
--- Week4/Exercise 7/BookstoreAPI/src/main/java/com/example/BookstoreAPI/service/CustomerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookstoreAPI.service;
-
-public class CustomerNotFoundException extends RuntimeException {
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
-
